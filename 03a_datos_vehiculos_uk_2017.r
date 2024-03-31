c37d36a36ed8ae64ee874742726f67e3c712be23
Pedro Concejero <pedro.concejerocerezo@gmail.com>
2024-03-31 22:12:46 +0200

    Add files via upload

---
+++ 03a_datos_vehiculos_uk_2017.r
@@ -0,0 +1,29 @@
+# Master Big Data UNED 2024
+# Datos vehículos UK 2017
+
+setwd("/home/pedro/Escritorio/UNED_2024/UNED_master_visualizacion")
+
+library(ggplot2)
+
+list.files()
+
+load("datos_4510_vehiculos_2016.rda")
+
+summary(datos)
+
+barplot(table(datos$FuelType))
+barplot(table(datos$Tipo))
+
+tipos_permitidos <- c("Diesel",
+                      "Gasolina",
+                      "Híbrido")
+
+datos_def <- datos[datos$Tipo %in% tipos_permitidos, ]
+
+summary(datos_def)
+barplot(table(datos_def$Tipo))
+
+plot(datos_def$EngineCapacity,
+     datos_def$MetricCombined)
+
+boxplot(datos_def$CO2gkm ~ datos_def$Tipo)
