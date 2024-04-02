6e1b4eaf84f3b2a1ee1caeb3d40acb84a7b8d67c
Pedro Concejero <pedro.concejerocerezo@gmail.com>
2024-04-02 20:55:02 +0200

    Add files via upload

---
+++ 07a_plotly_en_R.r
@@ -0,0 +1,45 @@
+# plotly en R
+# Para Curso visualización avanzada master UNED 2024
+
+# treemap para plotly 
+
+library(plotly)
+library(treemapify)
+library(ggplot2)
+library(tidyverse)
+
+load("datos_4510_vehiculos_2016.rda")
+
+# Un boxplot
+
+fig1 <- plot_ly(datos, 
+               x = ~MetricCombined, 
+               color = ~Tipo, 
+               type = "box")
+
+fig1
+
+# Un scatterplot
+
+fig2 <- plot_ly(datos, 
+                x = ~EngineCapacity,
+                y = ~MetricCombined, 
+                color = ~Tipo, 
+                type = "scatter")
+
+fig2
+
+# Data Labels on Hover
+
+
+fig3 <- plot_ly(datos,
+                x = ~EngineCapacity,
+                y = ~MetricCombined, 
+                color = ~Tipo, 
+                type = "scatter", 
+                # Hover text:
+    text = ~paste("Manufacturer: ", Manufacturer, Tipo)
+)
+
+fig3
+
